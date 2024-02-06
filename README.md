594d78dfeaf88a8ddc2a330a92a26498e7d62f6e
nick <nrs3088@rit.edu>
2024-02-06 15:20:00 -0500

    added motor libs

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 RMET 565 Capstone Project
 
 Seed plug tray dispensing robot. 
+
+
+NEVER EVER USE ARDUINO'S IDE OR CLI 
+https://github.com/microsoft/vscode-arduino/issues/438
+WASTE OF LIFE, SUCKER OF SOULS
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "MotorDriver/src/MotorDriver.h"
 
 // put function declarations here:
 int myFunction(int, int);
@@ -6,15 +7,18 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  Serial.print(result);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly:w
+
+  // just Blink.ino:
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
-
+  Serial.print("Blink\n");
 }
 
 // put function definitions here:
