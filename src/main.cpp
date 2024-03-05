11f8042c94eddf67a03b651c12f9b8d0cb47e693
nick <nrs3088@rit.edu>
2024-03-04 19:51:55 -0500

    added CoC

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ Class of 2024, RMET
 Author: Nick Spears
 */
 
-
+#include <Arduino.h>
 #include <AccelStepper.h>
 #include <LiquidCrystal.h> 
 #include <Wire.h>
@@ -39,24 +39,30 @@ AccelStepper initStepper(int indx){
         {0, 0},                  // Stepper 2: Reservoir metering motor pins
         {0, 0}                   // Stepper 3: Auger motor pins
         };
-
-    AccelStepper stepper(AccelStepper::DRIVER, PinDict[indx].dirPin, PinDict[indx].stepPin);             
-    // for a4988 (Bipolar, constant current, step/direction driver)
-    
+    AccelStepper stepper(AccelStepper::DRIVER, PinDict[indx].dirPin, PinDict[indx].stepPin);
+    // for Bipolar, constant current, step/direction driver
     return stepper;
 }
 
 void loop()                     // main 
 {
     // Initalize stepper motor objects:
-    AccelStepper stepper0 = initStepper(0);                                                         // Stepper0 object (X-Axis motor)
-    AccelStepper stepper1 = initStepper(1);                                                         // Stepper1 object (Y-Axis motor)
-    AccelStepper stepper2 = initStepper(2);                                                         // Stepper2 object (Reservoir metering motor)
-    AccelStepper stepper3 = initStepper(3);                                                         // Stepper3 object (Auger motor)
+    AccelStepper stepper0 = initStepper(0);                                                         // Stepper0 (X-Axis motor)
+    AccelStepper stepper1 = initStepper(1);                                                         // Stepper1 (Y-Axis motor)
+    AccelStepper stepper2 = initStepper(2);                                                         // Stepper2 (Reservoir metering motor)
+    AccelStepper stepper3 = initStepper(3);                                                         // Stepper3 (Auger motor)
 
-    stepper0.setMaxSpeed(100000);   // this limits the value of setSpeed(). Raise it if you like.
-    stepper0.setSpeed(90000);	   // runSpeed() will run the motor at this speed - set it to whatever you like.
-    stepper0.runSpeed();   // This will run the motor forever.
+    stepper0.setMaxSpeed(10000);                           // limits setSpeed(), measured in steps / second
+    stepper0.setSpeed(2000);	                            // runSpeed() will run the motor at this speed, measured in steps / second
+    stepper0.setAcceleration(1000);                         // motor acceleration in steps/s^2
+    stepper0.disableOutputs();                              // sets pins to LOW and prevents motors from drawing current 
+    stepper0.setCurrentPosition(0);                         // sets current position to 0 steps after switch trigger
+    delay(1);
+    stepper0.enableOutputs();                               // sets all pins 
 
-    return;
+    while (1) {
+        stepper0.runSpeed();
+    } 
+
+    exit (0);
 }
