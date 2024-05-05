b0f4aacfcf198f14d23a718fdc4c62406143a87d
nick <nrs3088@rit.edu>
2024-05-04 23:50:49 -0400

    fix: HomeAxis crashing.

--- lib/HomeAxis/src/HomeAxis.cpp
+++ lib/HomeAxis/src/HomeAxis.cpp
@@ -8,32 +8,42 @@ long HomeAxis(AccelStepper& stepper, Encoder& encoder, InputDebounce& switchObj,
     stepper.setMaxSpeed(homeVel*rotDir*1.2);
     stepper.setSpeed(homeVel*rotDir);
     
-    Serial.println("Running stepper...");
+
     // Move in direction of home:
+    Serial.println("Running stepper...");
     while (Debounce(switchObj) != 1) {           // Testing:: check if switch is active high or low in circuit, result bit still is 1..
         stepper.runSpeed();
         encoderPos = EncoderSteps(encoder, encoderPos);
     }
-    Serial.println("Switch triggered");
+    Serial.println("Switch triggered...");
     // Switch has been hit, reset positions:
     encoderPos = 0;
     stepper.setCurrentPosition(0);
+    
+
     // Move off by 80 steps, return and wait for switch to be hit:
+    Serial.println("Moving -80 steps...");
     stepper.moveTo(-80*rotDir);
+    stepper.setSpeed(homeVel*rotDir);
     delay(200);
-    stepper.setSpeed(homeVel*rotDir/1.5);
     while ((stepper.distanceToGo() != 0) && (Debounce(switchObj) != 1)) {stepper.run();}
+    
+
     // Send back to home:
+    Serial.println("Moving +100 steps, expecting to hit switch 2nd time...");
     stepper.moveTo(100*rotDir);
+    stepper.setSpeed(homeVel*rotDir);
     delay(200);
     while ((stepper.distanceToGo() != 0) && (Debounce(switchObj) != 1)) {stepper.run();}
+
     // Has hit home again, move off 60 steps and return encoder position:
-    stepper.moveTo(60*rotDir);
-    encoderPos = 0;
-    while (stepper.distanceToGo() != 0) {
-        stepper.run();
-        encoderPos = EncoderSteps(encoder, encoderPos);
-    }    
+    // stepper.moveTo(60*rotDir);
+    // encoderPos = 0;
+    // while (stepper.distanceToGo() != 0) {
+    //     stepper.run();
+    //     encoderPos = EncoderSteps(encoder, encoderPos);
+    // }    
+    
     Serial.println("Ending function, stepper pos: " + String(stepper.currentPosition()) + " encoder pos: " + encoderPos);
     return stepper.currentPosition(), encoderPos;
 }
